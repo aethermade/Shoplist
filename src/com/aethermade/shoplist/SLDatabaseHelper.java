270e26742a76f10f021ba44ff71863e3d5cc80eb
Anthony Kirkpatrick <a.x.kirkpatrick@googlemail.com>
2012-03-19 16:53:24 -0400

    Implemented demo of lifecycle activities

--- src/com/aethermade/shoplist/SLDatabaseHelper.java
+++ src/com/aethermade/shoplist/SLDatabaseHelper.java
@@ -12,18 +12,18 @@ public class SLDatabaseHelper extends SQLiteOpenHelper {
 	
 	public SLDatabaseHelper(Context context, String name, CursorFactory factory, int version) {
 		super(context, file, null, version);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase arg0) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
