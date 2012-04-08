39a20f08ab0c2a049e634aaa84df8aad01f57133
Kevin Paris <kparis06@gmail.com>
2012-04-08 12:19:27 -0400

    ListViem implimented with array

--- src/com/aethermade/shoplist/Shoplist.java
+++ src/com/aethermade/shoplist/Shoplist.java
@@ -3,6 +3,7 @@ package com.aethermade.shoplist;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class Shoplist extends Activity {
@@ -17,6 +18,14 @@ public class Shoplist extends Activity {
     	//this is where i am going to put ListView stuff to try    	
     	ListView listView = (ListView) findViewById(R.id.homelistview);
     	//Cursor c = 
+    	String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+    		"Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+    		"Linux", "OS/2","herpies" };
+    	ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+    			android.R.layout.simple_list_item_1, android.R.id.text1, values);
+    	listView.setAdapter(adapter);
+    	//The array list will be replaced with a cursor object to pull information from the database that is being set up from the content manager
+    	//This is where the onClickListener goes so you can see the contents of the list that you are selecting
     }
     
     public void onResume(){
